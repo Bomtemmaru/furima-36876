9ecbca820c0fe38a09cb327149c3d2c8ee624361
Bomtemmaru <reotang0405@gmail.com>
2021-10-05 14:43:57 +0900

    商品一覧表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   # before_action :move_to_index, except: [:index]
 
   def index
-    # @item = Item.includes(:user)
+    @item = Item.includes(:user).order("created_at DESC")
   end
 
   def new
